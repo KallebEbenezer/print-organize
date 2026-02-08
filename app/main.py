cbf68ac04636deda8a2c34fcc2484401a710ffda
KallebEbenezer <liradev58@gmail.com>
2026-02-08 19:35:35 -0300

    Initial settings of project

---
+++ app/main.py
@@ -0,0 +1,10 @@
+import sys
+from PySide6.QtWidgets import QApplication, QLabel
+
+app = QApplication(sys.argv)
+
+label = QLabel("Print Auto Save - App iniciado")
+label.resize(300, 100)
+label.show()
+
+sys.exit(app.exec())
