c6f493123bf09ae9a31def88c2dda59487c2f46a
KallebEbenezer <liradev58@gmail.com>
2026-02-08 20:19:48 -0300

    update database.py

--- app/database.py
+++ app/database.py
@@ -1,11 +1,3 @@
-import sqlite3
-from pathlib import Path
-
-DB_PATH = Path(__file__).resolve().parent.parent / "data" / "app.db"
-
-def get_connection():
-    return sqlite3.connect(DB_PATH)
-
 def init_db():
     conn = get_connection()
     cursor = conn.cursor()
@@ -13,7 +5,17 @@ def init_db():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS areas (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT UNIQUE NOT NULL
+            name TEXT NOT NULL
+        )
+    """)
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS disciplines (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            area_id INTEGER NOT NULL,
+            name TEXT NOT NULL,
+            save_path TEXT,
+            FOREIGN KEY (area_id) REFERENCES areas(id)
         )
     """)
 
