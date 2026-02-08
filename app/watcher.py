f9dfd8e4d8666a5dbf7467fa0eaa340da93df6c9
KallebEbenezer <liradev58@gmail.com>
2026-02-08 20:11:35 -0300

    debugging errors in watcher.py

--- app/watcher.py
+++ app/watcher.py
@@ -1,5 +1,6 @@
 from watchdog.events import FileSystemEventHandler
 from PySide6.QtCore import QObject, Signal
+import traceback
 
 class ScreenshotSignal(QObject):
     screenshot = Signal(str)
@@ -9,8 +10,16 @@ signal_bus = ScreenshotSignal()
 class ScreenshotHandler(FileSystemEventHandler):
 
     def on_created(self, event):
-        if event.is_directory:
-            return
+        try:
+            print("EVENTO DETECTADO:", event.src_path)
 
-        if event.src_path.lower().endswith(".png"):
-            signal_bus.screenshot.emit(event.src_path)
+            if event.is_directory:
+                return
+
+            if event.src_path.lower().endswith(".png"):
+                print("PRINT DETECTADO")
+                signal_bus.screenshot.emit(event.src_path)
+
+        except Exception:
+            print("ERRO NO WATCHER:")
+            traceback.print_exc()
