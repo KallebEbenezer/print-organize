ec7497b688c8bf70f5b96352766433eea82ea385
KallebEbenezer <liradev58@gmail.com>
2026-02-08 20:20:36 -0300

    new database model: discipline

--- app/models.py
+++ app/models.py
@@ -31,3 +31,49 @@ class Area:
         )
         conn.commit()
         conn.close()
+
+class Discipline:
+
+    @staticmethod
+    def create(area_id: int, name: str, save_path: str = None):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "INSERT INTO disciplines (area_id, name, save_path) VALUES (?, ?, ?)",
+            (area_id, name, save_path)
+        )
+
+        conn.commit()
+        conn.close()
+
+    @staticmethod
+    def list_by_area(area_id: int):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        rows = cursor.execute(
+            """
+            SELECT id, name, save_path
+            FROM disciplines
+            WHERE area_id = ?
+            ORDER BY id DESC
+            """,
+            (area_id,)
+        ).fetchall()
+
+        conn.close()
+        return rows
+
+    @staticmethod
+    def delete(discipline_id: int):
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        cursor.execute(
+            "DELETE FROM disciplines WHERE id = ?",
+            (discipline_id,)
+        )
+
+        conn.commit()
+        conn.close()
