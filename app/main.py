33eb2e3aff0143e5b897cf37737383c3a636a256
KallebEbenezer <liradev58@gmail.com>
2026-02-08 20:07:05 -0300

    solving break loop problem in popup -main.py

--- app/main.py
+++ app/main.py
@@ -1,13 +1,40 @@
 import sys
+import threading
+import time
 from PySide6.QtWidgets import QApplication
+from watchdog.observers import Observer
 
 from database import init_db
-from ui.areas_view import AreasView
+from watcher import ScreenshotHandler, signal_bus
+from popup import Popup
+
+SCREENSHOTS_DIR = "/home/ebenezer/Pictures/Screenshots"
+
+def start_watcher():
+    observer = Observer()
+    observer.schedule(ScreenshotHandler(), SCREENSHOTS_DIR, recursive=False)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        observer.stop()
+
+    observer.join()
 
 if __name__ == "__main__":
     init_db()
 
     app = QApplication(sys.argv)
-    window = AreasView()
-    window.show()
+
+    def open_popup(path):
+        popup = Popup(path)
+        popup.show()
+
+    signal_bus.screenshot.connect(open_popup)
+
+    watcher_thread = threading.Thread(target=start_watcher, daemon=True)
+    watcher_thread.start()
+
     sys.exit(app.exec())
